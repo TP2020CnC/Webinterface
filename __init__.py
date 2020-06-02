b1d6f9bf1a67001bb710ce8dc4454a583d8dbb37
rolandschwepp <rolandschwepp@gmail.com>
2020-06-02 08:58:04 +0200

    1

--- __init__.py
+++ __init__.py
@@ -6,5 +6,5 @@ from flask import Flask
 app = Flask(__name__)
 
 import Webinterface.views
-import Webinterface.library.SteuerungGenerieren
+import Webinterface.library.AnfragenVerarbeiten
 import Webinterface.library.KartenParser
