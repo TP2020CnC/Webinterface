44bb203a909bb55799666a83a0bceda4bfaa5d58
rolandschwepp <rolandschwepp@gmail.com>
2020-06-02 12:19:50 +0200

    1

--- library/AnfragenVerarbeiten.py
+++ library/AnfragenVerarbeiten.py
@@ -1,3 +1,17 @@
+#
+# Name: views.py
+# Projekt: FS4V Abschlussprojekt Staubsaugerroboter
+# Schule: Heinrich-Emanuel-Merck-Schule
+# Betrieb: COUNT+CARE
+# Autor: Robin Schepp
+# Co. Autor: Yannik Seitz
+# Erstellt: 2020-05-20
+# Letzte Änderung: 2020-06-2
+#
+# Der AnfrageVerarbeiter ist zentrales Element unserer Architektur und verarbeitet sowohl Anfragen von lokalen Programmen, wie
+# auch vom ESP und vom Roboter selbst. 
+# Von hier aus werden alle Programme bis auf die views.py aufgerufen und am laufen gehalten.
+
 import json
 import os
 import sqlite3
@@ -20,7 +34,7 @@ from Webinterface.library.KartenUpdater import KartenDebugger, KartenUpdater
 
 _KILL = True
 
-
+# Hält Updatevorgang am laufen -> Karte für Weboberfläche
 def Worker_KartenUpdater(path, url):
     """thread worker function"""
     ku = KartenUpdater(path, url)
@@ -65,22 +79,28 @@ class AnfrageVerarbeiter(object):
     """Diese Klasse generiert Aktionen, die auf Nutzereingaben zurückführen"""
 
     def __init__(self, currUrl):
+        # Roboter URL wird übergeben
         self.url = currUrl
+        # Die nächsten drei Zeilen wird die Aktuelle Statusmeldung angefragt und in ein Python Dictionary umgewandelt
         response = urlopen(self.url + "/api/current_status")
         string = response.read().decode('utf-8')
         self.currState = json.loads(string)
+        # Klassenvariablen werden initialisiert
         self.jobs = []
         self.payload = {}
+        # Der Header für die http PUT-Request zum senden des Fanmode (Robotermodus) wird initialisiert
         self.headers = {"content-type": "application/json",
                "Authorization": "<auth-key>"}
+        # Ein Datenbankverwalter zum Eintragen und Löschen der Datenbank
         self.db = database()
-
+    # Die Datei dreck.json wird geleert und so resettet
     def ResetDreckJson(self, path):
         data = {}
         data["dirt"] = []
         with open(path + "dreck.json", "w") as write_file:
             json.dump(data, write_file)
     
+    # Startet Kartenupdatevorgang
     def StartKartenUpdater(self, mapLocation, url):   
         global _KILL
         if _KILL == True:
@@ -92,6 +112,7 @@ class AnfrageVerarbeiter(object):
             #t2 = ThreadPool(target=Worker_KartenDebugger, args=(mapLocation,)) # DEBUG 
             #self.jobs.append(t2)
 
+    # Stoppt Kartenupdatevorgang
     def StopKartenUpdater(self):
         global _KILL
         _KILL = True
@@ -104,7 +125,8 @@ class AnfrageVerarbeiter(object):
         response = urlopen(self.url + "/api/current_status")
         string = response.read().decode('utf-8')
         self.currState = json.loads(string)
- 
+
+    #  Holt die gezippte Kartendatei in Bytecode zerlegt und speichert sie an angegebener Stelle
     def download_url(self, url, save_path):
         r = requests.get(url)
         open(save_path, 'wb').write(r.content)
@@ -113,6 +135,7 @@ class AnfrageVerarbeiter(object):
     # Die Daten werden über die HTTP Schnittstelle des Valetudo Webservers auf dem Roboter angefragt.
     # Verwendete lokale Dateien sind die karte.json und dreck.json 
     def verarbeiteAnfrage(self, method, robot_mode, myResponse="not_useable", mapBuilder="not_useable", mapFolder="not_useable", mapParser="not_usable", dbLocation="not_useable", request="not_useable"):
+        # Wenn es sich um eine PUT-Request handelt
         if method == "PUT":
             # startet Roboter
             if robot_mode == "start":
@@ -122,14 +145,20 @@ class AnfrageVerarbeiter(object):
             # stoppt Roboter
             elif robot_mode == "stop":
                 requests.put(self.url + "/api/stop_cleaning")
+            # pausiert Roboter
             elif robot_mode == "pause":
                 requests.put(self.url + "/api/pause_cleaning")
+            # Roboter macht auf sich aufmerksam
             elif robot_mode == "find":
                 requests.put(self.url + "/api/find_robot")
+            # Roboter fährt zum Dock zurück
             elif robot_mode == "home":
                 requests.put(self.url + "/api/drive_home")
+                # Tabelle mit Dreckdaten wird gelöscht
                 self.db.delTable(dbLocation)
+                # dreck.json wird geleert
                 self.ResetDreckJson(mapFolder)
+            # Die nächsten Zeilen tragen den Bodycontent für für PUT Anfrage zum setzen des Fanmode ein
             elif robot_mode == "whisper":
                 self.payload = {'speed': '1'}
             elif robot_mode == "quiet":
@@ -142,25 +171,34 @@ class AnfrageVerarbeiter(object):
                 self.payload = {'speed': '100'}
             elif robot_mode == "mop":
                 self.payload = {'speed': '105'}
+            # Testdreckdaten werden in Datenbank eingetragen
             elif robot_mode == "dustTest":
                 self.db.insertTestData(dbLocation)
+        # Wird ausgeführt, wenn es sich um eine GET-Request handelt
         elif method == "GET":
+            # Aktueller Status des Roboters wird Angefragt
             if robot_mode == "state":
                 self.getCurrentState()
+                # Antwort geht als Json-String zurück (wird von JavaScript auf der Website verarbeitet)
                 return jsonify({'battery': self.currState["battery"], 'clean_area': self.currState["clean_area"], 'fan_power': self.currState["fan_power"], 'human_state': self.currState["human_state"]})
-            elif robot_mode == "karte":
-                pass
+            # Liefert die gezippte Kartendatei mit allen Positionskoordinaten in Bytecode zerlegt zurück            
             elif robot_mode == "latest":
+                # Speichert die Datei vom Roboter als karte.gz
                 self.download_url((self.url + "/api/map/latest"), mapFolder + "karte.gz")
+            # Liefert die karte.json mit allen Koordinaten zurück -> Weboberfläche
             elif robot_mode == "map":
                 f = open(mapFolder + "karte.json", "r")
                 #return jsonify(json.loads(f.read()))
                 return f.read()
+        # Bei Postanfragen verwendet
         elif method == "POST":
+            # Hiermit werden Verschmutzungsdaten einzeln hingeschickt und gespeichert
             if robot_mode == "setDirt":
+                # Speichert Verschmutzungsdaten ab
                 self.db.updateDatabase(dbLocation, request.get_json(force=True))
+        # Wenn payload nicht leer ist (für den fanmode muss noch die request gesendet werden)
         if self.payload != "":
+            # Request wird gestellt -> Fanmode wird gesetzt
             requests.put((self.url + "/api/fanspeed"),data=json.dumps(self.payload), headers=self.headers)
+            # Wenn alles gut kommt ok gut zurück
             return jsonify({'message': "ok gut"})
-        else:
-            return jsonify({'message': "ok"})
